c2b87cce41ca2660a3bccf0cd5e6fb53ace83529
fafa <fabienpuech1@gmail.com>
2025-02-02 11:05:38 +0100

    edit: smear cursor

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -91,12 +91,12 @@ return {
   {
     "sphamba/smear-cursor.nvim",
     opts = {
-      {
-        stiffness = 0.8, -- 0.6      [0, 1]
-        trailing_stiffness = 0.5, -- 0.3      [0, 1]
-        distance_stop_animating = 0.5, -- 0.1      > 0
-        hide_target_hack = false,
-      },
+      cursor_color = "#c27aff",
+      legacy_computing_symbols_support = true,
+      hide_target_hack = false,
+      stiffness = 0.9,
+      trailing_stiffness = 0.3,
+      trailing_exponent = 1,
     },
   },
   {
